31e635b5331eeea9ca0e813eecea0e56e5a7766f
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2025-01-24 16:15:43 -0300

    Update: improve solution ´Merge Strings Alternately´in c++.

--- Arrays & Strings/Merge Strings Alternately.cpp
+++ Arrays & Strings/Merge Strings Alternately.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         std::string resultStr = "";
-        unsigned int i = 0;
-        while (i < word1.length() || i < word2.length())
-        {
-            if (i < word1.length())
-            {
+        int maxLength = max(word1.length(), word2.length());
+        for (size_t i = 0; i < maxLength; ++i) {
+            if (i < word1.length()) {
                 resultStr += word1[i];
             }
 
-            if(i < word2.length())
-            {
+            if (i < word2.length()) {
                 resultStr += word2[i];
             }
-            ++i;
         }
         return resultStr;
     }
-
 };
