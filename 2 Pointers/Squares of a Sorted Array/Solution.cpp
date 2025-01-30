a930aae69e2cb16d9c2c611023e00df342db108d
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2025-01-29 23:16:22 -0300

    update: optimize using one loop to add square calculation also sorting, using 2 pointers technique.

--- 2 Pointers/Squares of a Sorted Array/Solution.cpp
+++ 2 Pointers/Squares of a Sorted Array/Solution.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for (size_t i = 0; i < nums.size(); ++i) {
-            nums[i] = nums[i] * nums[i];
-        }
+        int left = 0;
+        int right = nums.size() - 1;
+        vector<int> result(nums.size());
+        int index = nums.size() - 1;
 
-        sort(nums.begin(), nums.end());
-        return nums;
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                result[index--] = leftSquare;
+                left++;
+            } else {
+                result[index--] = rightSquare;
+                right--;
+            }
+        }
+        return result;
     }
 };
